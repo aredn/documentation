258b8b9edf5e3408b7dbb0037334055f92586a83
kc0euw <44625233+kc0euw@users.noreply.github.com>
2018-11-25 15:47:55 -0700

    Updated conf.py with RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,8 @@ pygments_style = None
 #
 #html_theme = 'alabaster'
 #html_theme = 'sphinxdoc'
-html_theme = 'nature'
+#html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

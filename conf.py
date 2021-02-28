39b4702ae790fe8379f4da6bd384914479a841cd
ab7pa <ab7pa.radio@gmail.com>
2021-02-28 08:53:19 -0700

    Set March 2021 Release Date
    Set the doc release date to March 2021 so it corresponds to the next 
    code Stable Release of 3.21.3.0

--- conf.py
+++ conf.py
@@ -24,9 +24,9 @@ copyright = u'2021, Amateur Radio Emergency Data Network, Inc. Licensed under th
 author = u'Amateur Radio Emergency Data Network, Inc.'
 
 # The short X.Y version
-version = u'3.21.2.0'
+version = u'3.21.3.0'
 # The full version, including alpha/beta/rc tags
-release = u'3.21.2.0'
+release = u'3.21.3.0'
 
 
 # -- General configuration ---------------------------------------------------

430cffbffd257814fec8ec71977016cc7f3bde36
dman776 <dman776@gmail.com>
2021-11-13 12:10:58 -0600

    min sphinx version to 4.2 (#116)

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ release = u'3.21.4.0'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '4.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom

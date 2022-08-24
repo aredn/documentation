d157160eb1f1c14ef1060440d987ccee97075c8e
Steve Lewis AB7PA <ab7pa.radio@gmail.com>
2022-08-24 12:11:48 -0700

    prepare 3.22.8.0 release

--- conf.py
+++ conf.py
@@ -25,9 +25,9 @@ copyright = f'{date.today().year}, Amateur Radio Emergency Data Network, Inc. Li
 author = u'Amateur Radio Emergency Data Network, Inc.'
 
 # The short X.Y version
-version = u'latest'
+version = u'3.22.8.0'
 # The full version, including alpha/beta/rc tags
-release = u'latest nightly build'
+release = u'3.22.8.0'
 
 # -- General configuration ---------------------------------------------------
 

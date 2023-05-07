b583cf1e260e5d23939f5bea0887eff4d7c119d7
Steve <69524416+ab7pa@users.noreply.github.com>
2023-05-07 16:06:42 -0700

    update conf.py for new version (#255)

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ release = u'Latest Nightly Build'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '4.4'
+needs_sphinx = '6.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -71,7 +71,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -79,7 +79,7 @@ language = None
 exclude_patterns = [u'_build', u'Thumbs.db', u'.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------

819dfde66213b7820adc108e20d76c88b5a5e8ba
Steve Lewis AB7PA <ab7pa.radio@gmail.com>
2023-05-08 13:53:27 -0700

    remove sphinx version limitation

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ release = u'Latest Nightly Build'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '5.3'
+#needs_sphinx = '5.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom

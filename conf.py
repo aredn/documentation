877c0655e135dde306d0b854f6a6eb6e9244952b
AB7PA <ab7pa.radio@gmail.com>
2025-02-10 12:59:46 -0700

    pretag-3.25.2.0

--- conf.py
+++ conf.py
@@ -35,9 +35,9 @@ copyright = f'{date.today().year}, Amateur Radio Emergency Data Network, Inc. Li
 author = u'Amateur Radio Emergency Data Network, Inc.'
 
 # The short X.Y version
-version = u'latest'
+version = u'3.25.2.0'
 # The full version, including alpha/beta/rc tags
-release = u'latest'
+release = u'3.25.2.0'
 
 # -- General configuration ---------------------------------------------------
 
